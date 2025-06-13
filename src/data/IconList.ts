812fac726eeabb377c67ff4d4bac90053de0bf13
Maia-Th <thiagomaia633@gmail.com>
2025-06-13 11:16:53 -0300

    inicio do portfolio definitivo

---
+++ src/data/IconList.ts
@@ -0,0 +1,10 @@
+export const IconList = {
+  code: "/assets/images/icons/codigo.png",
+  site: "/assets/images/icons/site.png",
+  youtube: "/assets/images/icons/youtube.png",
+  linkedin: "/assets/images/icons/linkedin.png",
+  github: "/assets/images/icons/github.png",
+  whatsapp: "/assets/images/icons/whatsapp.png",
+  gmail: "/assets/images/icons/gmail.png",
+  cv: "/assets/images/icons/curriculum-vitae.png",
+};
